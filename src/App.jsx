523a11e62df915116e46a59a6ac97aa7e74eb623
lkrabac <lkrabac1@gmail.com>
2023-01-03 22:10:07 +0100

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <div className={styles.app}>
+      
+    </div>
+  )
+}
+
+export default App
