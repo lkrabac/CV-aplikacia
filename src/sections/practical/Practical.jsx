a61e0415a671562ae4d7039df8c30f873d62f277
lkrabac <lkrabac1@gmail.com>
2023-01-04 02:29:00 +0100

    Practical

--- src/sections/practical/Practical.jsx
+++ src/sections/practical/Practical.jsx
@@ -4,8 +4,25 @@ import styles from "./Practical.module.css";
 
 const Practical = () => {
   return (
-    <div>Practical</div>
-  )
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <h2>PRAX V PRÁCI</h2>
+        <form className={styles.form}>
+          <h3>Práca</h3>
+          <input
+            id="praca"
+            type="text"
+            placeholder="Softwarový inžinier"
+          />
+          <h3>Krátky popis práce</h3>
+          <textarea id="popis__prace" type="text" placeholder="..." />
+          <button id="submit-education" className={styles.submit}>
+            Pridať do zoznamu
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Practical
